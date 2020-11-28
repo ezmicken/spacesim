a428dc7074dbef2987886e7697353ccd4423c803
ezmicken <smoggach@gmail.com>
2020-11-28 18:04:43 -0500

    Add some helper functions for wrapping numbers

---
+++ pkg/spacesim/Helpers.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+  //"log"
+  "github.com/ezmicken/fixpoint"
+)
+
+func WrapQ16(val, min, length fixpoint.Q16) fixpoint.Q16 {
+  for val.N >= length.N { val = val.Sub(length) }
+  for val.N < min.N { val = val.Add(length) }
+  return val;
+}
+
+func WrapInt(val, min, length int) int {
+  for val >= length { val -= length }
+  for val < min { val += length }
+  return val
+}
+
+func WrapAngle(val int) int {
+  return WrapInt(val, 0, 360)
+}
+
+func WrapAngleQ16(val fixpoint.Q16) fixpoint.Q16 {
+  return WrapQ16(val, fixpoint.Q16FromFloat(float32(0)), fixpoint.Q16FromFloat(float32(360)))
+}
+
+func BitOn(b byte, pos int) bool {
+  return (b & (1 << pos)) != 0;
+}
+
---
+++ pkg/spacesim/Helpers_test.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+  "testing"
+  "github.com/ezmicken/fixpoint"
+)
+
+func TestWrapAngleQ16(t *testing.T) {
+  var v fixpoint.Q16
+  for i := 1; i < 360; i++ {
+    v = WrapAngleQ16(fixpoint.Q16FromFloat(float32(360) + float32(i)))
+    if v != fixpoint.Q16FromFloat(float32(i)) {
+      t.Logf("Failed! %v is not %v", v, float32(i))
+      t.Fail()
+    }
+    v = WrapAngleQ16(fixpoint.Q16FromFloat(float32(0) - float32(i)))
+    if v != fixpoint.Q16FromFloat(float32(360 - i)) {
+      t.Logf("Failed! %v is not %v", v, float32(360 - i))
+      t.Fail()
+    }
+  }
+}
