5c05103827323af4d62604defe82f5400dcbd85b
ezmicken <smoggach@gmail.com>
2020-11-29 18:14:12 -0500

    Simulation no longer assigning Ids.

--- pkg/spacesim/Simulation.go
+++ pkg/spacesim/Simulation.go
@@ -9,13 +9,12 @@ type Simulation struct {
   controlledBodies    sync.Map
   bodiesById          sync.Map
   allBodies           []*Body
-  nextId              int32
 
   seq                 uint16
   timestep            fixpoint.Q16
 }
 
-var rotationSpeed int = 9
+var rotationSpeed int32 = 9
 var thrust fixpoint.Q16 = fixpoint.Q16FromFloat(float32(0.36))
 var maxSpeed fixpoint.Q16 = fixpoint.Q16FromFloat(float32(20))
 
@@ -24,11 +23,10 @@ func NewSimulation(ts fixpoint.Q16) *Simulation {
   s.timestep = ts
   s.seq = 0
   s.allBodies = []*Body{}
-  s.nextId = 0
   return &s
 }
 
-func (s *Simulation) GetControlledBody(id int32) *ControlledBody {
+func (s *Simulation) GetControlledBody(id uint16) *ControlledBody {
   cb, ok := s.controlledBodies.Load(id)
   if ok {
     return cb.(*ControlledBody)
@@ -37,7 +35,7 @@ func (s *Simulation) GetControlledBody(id int32) *ControlledBody {
   }
 }
 
-func (s *Simulation) GetBody(id int32) *Body {
+func (s *Simulation) GetBody(id uint16) *Body {
   b, ok := s.bodiesById.Load(id)
   if ok {
     return b.(*Body)
@@ -46,17 +44,15 @@ func (s *Simulation) GetBody(id int32) *Body {
   }
 }
 
-func (s *Simulation) AddControlledBody() int32 {
+func (s *Simulation) AddControlledBody(id uint16) {
   cb := NewControlledBody(rotationSpeed, thrust, maxSpeed, s)
-  id := s.nextId
-  s.nextId++
   s.allBodies = append(s.allBodies, cb.GetBody())
   s.controlledBodies.Store(id, cb)
   s.bodiesById.Store(id, cb.GetBody())
-  return id
+  return
 }
 
-func (s *Simulation) RemoveControlledBody(id int32) {
+func (s *Simulation) RemoveControlledBody(id uint16) {
   cb, ok := s.controlledBodies.Load(id)
   if ok && cb != nil {
     cb.(*ControlledBody).GetBody().Kill()
@@ -65,13 +61,20 @@ func (s *Simulation) RemoveControlledBody(id int32) {
   }
 }
 
-func (s *Simulation) Advance(seq int) {
-  // Advance the simulation by one step for each controlled body
-  s.controlledBodies.Range(func(key, value interface{}) bool {
-    value.(*ControlledBody).Advance(seq)
-    return true
-  })
+func (s *Simulation) AdvanceControlledBody(id, seq uint16) {
+  cb := s.GetControlledBody(id)
+  if cb != nil {
+    cb.Advance(seq)
+  }
+}
+func (s *Simulation) AdvanceBody(id, seq uint16) {
+  b := s.GetBody(id)
+  if b != nil {
+    b.Advance(seq)
+  }
+}
 
+func (s *Simulation) Advance(seq int) {
   // Update all bodies
   // flag dead bodies for removal
   // process live bodies
