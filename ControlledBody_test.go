e4be08724981f637f0daa36477b7ba4020acd47f
ezmicken <smoggach@gmail.com>
2020-12-30 15:45:52 -0500

    Add a test to spin the statebuffer around a few times via ControlledBody.

--- ControlledBody_test.go
+++ ControlledBody_test.go
@@ -12,3 +12,25 @@ func TestAddBlock(t *testing.T) {
     cb.AddBlock(i, i)
   }
 }
+
+func TestAdvance(t *testing.T) {
+  sim := NewSimulation(fixpoint.ZeroQ16, fixpoint.Q16FromInt32(32))
+  cb := NewControlledBody(9, 0, fixpoint.Q16FromFloat(0.36), fixpoint.Q16FromFloat(20), sim)
+
+  var ht HistoricalTransform
+  ht.Seq = uint16(0)
+  ht.Angle = 0
+  ht.AngleDelta = 0
+  ht.Position = fixpoint.ZeroVec3Q16
+  ht.Velocity = fixpoint.ZeroVec3Q16
+  ht.VelocityDelta = fixpoint.ZeroVec3Q16
+
+  cb.Initialize(ht)
+
+  testSeq := uint16(12)
+  for i := uint16(0); i < 640; i++ {
+    cb.PushInput(testSeq, byte(4))
+    testSeq++
+    cb.Advance(i)
+  }
+}
